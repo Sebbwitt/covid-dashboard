c8c64de443d90edf313e997dea6c2273f3bb4a5e
sebbwitt <sebastian.khoo26@gmail.com>
2020-06-12 00:18:32 +0800

    Updates

---
+++ precache-manifest.4bc6f6bc883bec77557cc9bdbc1dd295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a597896916395639e477bd296c4c22",
+    "url": "/covid-dashboard/index.html"
+  },
+  {
+    "revision": "e1b4295b6980c4fae08a",
+    "url": "/covid-dashboard/static/css/main.644c7997.chunk.css"
+  },
+  {
+    "revision": "5947a3d8e66bb9c0ebd7",
+    "url": "/covid-dashboard/static/js/2.af5a7619.chunk.js"
+  },
+  {
+    "revision": "64f24e5a999c7daa34aca2527f09b7c2",
+    "url": "/covid-dashboard/static/js/2.af5a7619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1b4295b6980c4fae08a",
+    "url": "/covid-dashboard/static/js/main.ac345fe8.chunk.js"
+  },
+  {
+    "revision": "ec08beda41eb041e1972",
+    "url": "/covid-dashboard/static/js/runtime-main.33452e2a.js"
+  }
+]);
